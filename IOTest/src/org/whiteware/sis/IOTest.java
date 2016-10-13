5a6597d5efd4d9498592b00eb059c5b4120afc31
Alan White <alan@whitemail.net>
2016-10-13 16:31:42 +0200

    Initial commit

---
+++ IOTest/src/org/whiteware/sis/IOTest.java
@@ -0,0 +1,135 @@
+package org.whiteware.sis;
+
+import java.awt.Dimension;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
+
+
+public class IOTest {
+
+	final JFrame frame = new JFrame("IO Test IPC Example");
+	private JTextArea textArea;
+	/**
+	 * Constructs the IOTest app, it assumes it is the master, given it has been created!
+	 * @param fileName
+	 */
+	public IOTest(String fileName) {
+		try {
+			final JFrame frame = new JFrame("IO Test IPC Example");
+			textArea = new JTextArea();
+			JScrollPane scrollPane = new JScrollPane(textArea); 
+			textArea.setEditable(false);
+			textArea.append("File Names Requested\n--\n");
+			frame.add(textArea);
+			frame.setMinimumSize(new Dimension(640, 480));
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.pack();
+			frame.setLocationRelativeTo(null);
+			frame.setVisible(true);
+
+			if ( fileName != null )
+				handleFileOpen(fileName);
+
+			new FileNameListener().execute();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void handleFileOpen(String fileName) {
+		textArea.append(fileName+"\n");
+		frame.toFront();
+		frame.requestFocus();
+	}
+
+	class FileNameListener extends SwingWorker<Integer, String> {
+
+		@Override
+		protected Integer doInBackground() throws Exception {
+			ServerSocket serverSocket = new ServerSocket(0, 50, InetAddress.getLoopbackAddress());
+			int portNum=serverSocket.getLocalPort();
+			publish("Port:"+portNum);
+
+			while(!serverSocket.isClosed()) {
+				Socket client = serverSocket.accept();
+
+				BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				String message = in.readLine();
+				publish("File:"+message);
+
+				in.close();
+				client.close();
+			}
+			return 1;
+		}
+
+		@Override
+		protected void process(List<String> chunks) {
+			try {
+				for ( String message :chunks) {
+					String[] words = message.split(":");
+					switch(words[0]) {
+					case "Port":
+						int portNum = Integer.parseInt(words[1]);
+						System.out.println("Setting port number "+portNum);
+						SingleInstanceSupport.setPortNumber(portNum);
+						break;
+					case "File":
+						handleFileOpen(words[1]);
+						break;
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Launches the program if the master instance otherwise pass the arguments via the socket to the master
+	 * To allow of master instance startup time, a short timer based back-off delay is used when sending the filename
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+
+		if ( SingleInstanceSupport.isMasterInstance() ) {
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					new IOTest(args.length > 0 ? args[0] : null);
+				}
+			});
+		} else {
+			int port = 0;
+			int sleepTime = 500;
+			while ( port == 0 || sleepTime < 5000) {
+				try {
+					port = SingleInstanceSupport.getMasterPort();
+				} catch (Exception e) {
+					Thread.sleep(sleepTime *= 2);
+				}
+			}
+			Socket cc = new Socket(InetAddress.getLoopbackAddress(), port);
+			PrintWriter out = new PrintWriter(cc.getOutputStream(), true);
+			out.println(args.length > 0 ? args[0] : "Dummy Message");
+			cc.close();
+		}
+
+
+	}
+
+
+}
---
+++ IOTest/src/org/whiteware/sis/SingleInstanceSupport.java
@@ -0,0 +1,112 @@
+package org.whiteware.sis;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+
+
+public class SingleInstanceSupport {
+	/**
+	 * Explicitly name file offsets for each of the 3 ints stored within
+	 */
+	private final static int countIntBytes 				= Integer.SIZE / 8;
+	private final static int lockIntPosition 			= 0 * countIntBytes;
+	private final static int portWrittenIntPosition 	= 1 * countIntBytes;
+	private final static int portIntPosition 			= 2 * countIntBytes;
+
+	private final static String LOCKFILE = "IOTestFile.tmp";
+	private static File lockFile;
+	private static FileChannel fileChannel = null;
+	private static FileLock masterLock = null;
+
+
+	/**
+	 * Obtains the port number the master instance is listening on
+	 * @return the port number to connect to or 0 if none available
+	 */
+	public static int getMasterPort() throws Exception {
+
+		String userHome = System.getProperty("user.home");
+		if ( userHome == null ) {
+			throw(new Exception("no home folder"));
+		}
+
+		String lockFilename = userHome + File.separator + LOCKFILE;
+		File f = new File(lockFilename);
+
+		FileChannel fileChannel = new RandomAccessFile(f, "r").getChannel();
+		ByteBuffer intBuf = ByteBuffer.allocate(3*countIntBytes);
+		intBuf.clear();
+
+		int numRead = fileChannel.read(intBuf);
+		if ( numRead < countIntBytes ) {
+			throw(new Exception("no port data in file yet"));
+		}
+
+		int portNumber = 0;
+		int portWritten = intBuf.getInt(portWrittenIntPosition);
+		if ( portWritten == 1 ) {
+			portNumber = intBuf.getInt(portIntPosition);
+		} else {
+			throw(new Exception("port not yet written to file"));
+		}
+
+		return portNumber;
+	}
+
+	/**
+	 * Checks if we can get an exclusive lock on a well known region of the filename.
+	 * Deliberately leaves the lockFile open - not  resource leak as long as you only call this once!
+	 * @return true if can own the lock
+	 */
+	public static boolean isMasterInstance() throws Exception {
+
+		String userHome = System.getProperty("user.home");
+		if ( userHome == null ) {
+			throw(new Exception("no home folder"));
+		}
+
+		lockFile = new File(userHome + File.separator + LOCKFILE);
+		fileChannel = new RandomAccessFile(lockFile, "rwd").getChannel();
+
+		// obtain exclusive lock on the first int in the file to become the master
+		masterLock = fileChannel.tryLock(lockIntPosition,countIntBytes,false);
+		if ( masterLock == null ) {
+			fileChannel.close();
+			return false;
+		}
+
+		lockFile.deleteOnExit();
+
+		ByteBuffer intBuf = ByteBuffer.allocate(3*countIntBytes);
+		for ( int i=0; i<3; i++)
+			intBuf.putInt(0);
+		fileChannel.write(intBuf, 0);
+
+		return true;
+
+	}
+
+	/**
+	 * Updates the lock file with the port number to use to communicate with the master instance
+	 * @param port
+	 */
+	public static void setPortNumber(int port) throws Exception {
+
+		if ( fileChannel == null )
+			throw(new Exception("lock file not opened"));
+
+		ByteBuffer intBuf = ByteBuffer.allocate(3*countIntBytes);
+		intBuf.putInt(portIntPosition, port);
+		intBuf.rewind();
+		fileChannel.write(intBuf, 0);
+
+		int portWritten = 1;
+		intBuf.putInt(portWrittenIntPosition, portWritten);
+
+		intBuf.rewind();
+		fileChannel.write(intBuf, 0);
+	}
+}
